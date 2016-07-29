8fd0063ca8dd0d4150e5b63faaa1ea459dff6468
craftweak <craftweak@gmail.com>
2016-07-29 16:46:03 +0800

    first commot

---
+++ Assets/New Folder/FollowCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+	public GameObject target = null;
+	public bool orbitY = false;
+
+	private Vector3 positionOffset = Vector3.zero;
+
+	private void Start()
+	{
+		positionOffset = transform.position -  target.transform.position;
+	}
+
+	
+
+	private void Update () {
+	
+		if (target != null)
+		{
+			transform.LookAt (target.transform);
+
+			if (orbitY)
+				transform.RotateAround (target.transform.position, Vector3.up, Time.deltaTime * 15);
+
+			transform.position = target.transform.position + positionOffset;
+		}
+	}
+}
---
+++ Assets/New Folder/WalkSpeed.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+	
+
+
+
+
+public class WalkSpeed : MonoBehaviour {
+
+
+
+	Animator animator;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		speed = 0.5f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (speed < 0)
+			speed = 0;
+		if (speed > 1)
+			speed = 1;
+		animator.SetFloat ("Walk", speed);
+	}
+}
